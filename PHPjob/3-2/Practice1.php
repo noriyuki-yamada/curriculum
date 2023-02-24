18fc03363d1e349b254d9a178b4d173b812091e3
noriyuki-yamada <125259694+noriyuki-yamada@users.noreply.github.com>
2023-02-24 12:34:42 +0900

    課題3-2
    再提出用修正版

--- PHPjob/3-2/Practice1.php
+++ PHPjob/3-2/Practice1.php
@@ -1,27 +1,26 @@
 <?php
 
 //りんご、みかん、桃のフルーツの単価と個数をもとに料金を計算します
-$sum = 0;
 
 //step1:フルーツと単価の連想配列を作成してください。
 //配列の0:リンゴ、1:みかん、2:桃の順に個数を配列で作成してください。
-$FruitsCost = ["りんご" => 300 , "みかん" => 150 , "もも" => 3000];
-$FruitsQuantity = ["りんご" => 4 , "みかん" => 8 , "もも" => 1];
+$FruitsCost = ["りんご" => 100 , "みかん" => 75 , "もも" => 300];
+$FruitsQuantity = [3,2,10];
+//単価、個数再設定しました。
 
 //step2:単価を計算する関数を定義してください。
 //引数はフルーツの単価・個数の２つ、返り値は計算した合計値を返します。
-function getPrice($cost, $quantity){
-    return $cost * $quantity;
+function getPrice($単価, $個数){
+    $合計値 = $単価 * $個数;
+    return $合計値;
 }
 
 //step3:繰り返しを使ってそれぞれのフルーツを書き出してください。
+$i = 0;
 foreach($FruitsCost as $key => $value){
-    $price = getPrice($FruitsCost[$key] ,$FruitsQuantity[$key]);
-    $sum += $price;
-    echo "$key の価格は $price 円です。";
-    echo '<br>';
+    echo $key."は".getPrice($value,$FruitsQuantity[$i])."円です。";
+    echo'<br>';
+    $i++;
 }
-
-echo "合計金額は$sum 円です";
 
 ?>

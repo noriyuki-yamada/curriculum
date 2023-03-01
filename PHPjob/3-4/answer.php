6aae42d48fbf0f47988634f9584e7d4e0cfb875b
noriyuki-yamada <125259694+noriyuki-yamada@users.noreply.github.com>
2023-03-02 07:44:25 +0900

    課題3-4
    提出用

--- PHPjob/3-4/answer.php
+++ PHPjob/3-4/answer.php
@@ -1,15 +1,37 @@
-<?php 
+<link rel="stylesheet" href="css/answer.css">
+
+<?php
+
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
+$my_name = $_POST['my_name'];
+$CorrectPort = $_POST['CorrectPort'];
+$CorrectWeb = $_POST['CorrectWeb'];
+$CorrectMySQL = $_POST['CorrectMySQL'];
+$AnswerPort = $_POST['AnswerPort'];
+$AnswerWeb = $_POST['AnswerWeb'];
+$AnswerMySQL = $_POST['AnswerMySQL'];
 
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
+function judge($answer, $correct){
+    if($answer === $correct){
+        echo "正解！";
+    }elseif($answer !== $correct){
+        echo "残念・・・";
+    }
+}
 
 ?>
-<p><!--POST通信で送られてきた名前を表示-->さんの結果は・・・？</p>
-<p>①の答え</p>
-<!--作成した関数を呼び出して結果を表示-->
 
-<p>②の答え</p>
-<!--作成した関数を呼び出して結果を表示-->
+<p1><!--POST通信で送られてきた名前を表示--><?php echo $my_name; ?>さんの結果は・・・？</p1>
 
-<p>③の答え</p>
+<p2>①の答え</p2>
 <!--作成した関数を呼び出して結果を表示-->
+<p2> <?php judge($AnswerPort, $CorrectPort) ?> </p2>
+
+<p2>②の答え</p2>
+<!--作成した関数を呼び出して結果を表示-->
+<p2> <?php judge($AnswerWeb, $CorrectWeb) ?> </p2>
+
+<p2>③の答え</p2>
+<!--作成した関数を呼び出して結果を表示-->
+<p2> <?php judge($AnswerMySQL, $CorrectMySQL) ?> </p2>
--- PHPjob/3-4/question.php
+++ PHPjob/3-4/question.php
@@ -1,20 +1,63 @@
+<link rel="stylesheet" href="css/question.css">
+
 <?php
+
 //POST送信で送られてきた名前を受け取って変数を作成
+$my_name = $_POST['my_name'];
 
 //①画像を参考に問題文の選択肢の配列を作成してください。
+$Port = [80, 22, 20, 21];
+$Web = ["PHP", "Python", "JAVA", "HTML"];
+$MySQL = ["join", "select", "insert", "update"];
 
 //② ①で作成した、配列から正解の選択肢の変数を作成してください
+$CorrectPort = $Port[0];
+$CorrectWeb = $Web[3];
+$CorrectMySQL = $MySQL[1];
 
 ?>
-<p>お疲れ様です<!--POST通信で送られてきた名前を出力-->さん</p>
+
+<p>お疲れ様です<!--POST通信で送られてきた名前を出力--><?php echo $my_name;?>さん</p>
+
 <!--フォームの作成 通信はPOST通信で-->
-<h2>①ネットワークのポート番号は何番？</h2>
-<!--③ 問題のradioボタンを「foreach」を使って作成する-->
+<form action="answer.php" method="POST">
 
-<h2>②Webページを作成するための言語は？</h2>
-<!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <h2>①ネットワークのポート番号は何番？</h2>
 
-<h2>③MySQLで情報を取得するためのコマンドは？</h2>
-<!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <div class="radio">
+        <?php foreach($Port as $value){ ?>
+            <input type ="radio" name="AnswerPort" value="<?php echo $value; ?>">
+            <?php echo $value;
+        } ?>
+    </div>
 
-<!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+    <h2>②Webページを作成するための言語は？</h2>
+
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <div class="radio">
+        <?php foreach($Web as $value){ ?>
+            <input type ="radio" name="AnswerWeb" value="<?php echo $value; ?>">
+            <?php echo $value;
+        } ?>
+    </div>
+
+    <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <div class="radio">
+        <?php foreach($MySQL as $value){ ?>
+            <input type ="radio" name="AnswerMySQL" value="<?php echo $value; ?>">
+            <?php echo $value;
+        } ?>
+    </div>
+
+    <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+    <input type="hidden" name="CorrectPort" value="<?php echo $CorrectPort; ?>" />
+    <input type="hidden" name="CorrectWeb" value="<?php echo $CorrectWeb; ?>" />
+    <input type="hidden" name="CorrectMySQL" value="<?php echo $CorrectMySQL; ?>" />
+    <input type="hidden" name="my_name" value= "<?php echo $my_name; ?>" >
+
+    <br>
+    <div class="submit"><input type ="submit" value="回答する"></div>
+</form>
